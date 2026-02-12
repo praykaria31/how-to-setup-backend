4f4513688fa904771a1df4b70cb25fb1720bfea3
praykaria31 <praykaria3127@gmail.com>
2026-02-12 20:53:55 +0530

    util code disc

--- src/index.js
+++ src/index.js
@@ -1,44 +1,26 @@
 // require('dotenv').config({path:'./env'})
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 // import mongoose from "mongoose";
 // import {DB_NAME} from "./constants";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: './.env' 
-})
-
-
-
-
-
-
+  path: "./.env",
+});
 
 connectDB()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERRR: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed!!!", err);
+  });
 
 /*
 import express  from "express"
